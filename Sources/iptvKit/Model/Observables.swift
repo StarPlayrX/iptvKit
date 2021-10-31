d589a7c16cd6b87c30c326e5c306031886a31c1a
Todd Bruss <admin@starplayrx.com>
2021-10-31 18:54:44 -0400

    pretty good so far.

--- Sources/iptvKit/Model/Observables.swift
+++ Sources/iptvKit/Model/Observables.swift
@@ -52,7 +52,7 @@ public class PlayerObservable: ObservableObject {
     @Published public var fullscreen: Bool = false
     
     @Published public var previousStreamID: Int = -2
-    @Published public var previousCategoryID: String? = "-Cat"
+    @Published public var previousCategoryID: String = ""
     
     @Published public var channelName: String = ""
     @Published public var imageURL: String = ""
