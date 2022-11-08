3f126aed60a1b62bc0d8e395765475af88570e21
Todd Bruss <admin@starplayrx.com>
2022-11-08 15:35:05 -0500

    remove KSPlayer

--- KSPlayer/Tests/KSPlayerTests/VideoPlayerControllerTest.swift
+++
@@ -1,5 +0,0 @@
-@testable import KSPlayer
-import XCTest
-class VideoPlayerControllerTest: XCTestCase {
-    func testResize() {}
-}
--- KSPlayer/Tests/KSPlayerTests/VideoPlayerViewTest.swift
+++
@@ -1,5 +0,0 @@
-@testable import KSPlayer
-import XCTest
-class VideoPlayerViewTest: XCTestCase {
-    func testResize() {}
-}
